307be434d818fc6e8afb6187d932feb5a943d76e
Lucida409 <9d2s942t85@privaterelay.appleid.com>
2026-08-31 14:37:23 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Lucida Farm</h1>
+      <p>Site under construction 🐴</p>
+    </main>
+  );
+}
