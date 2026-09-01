0e8361778f2d2945eb9a95fab6005b0f2a1026fa
Lucida409 <9d2s942t85@privaterelay.appleid.com>
2026-09-01 10:35:17 +0200

    Update page.js

--- app/for-sale/page.js
+++ app/for-sale/page.js
@@ -3,6 +3,7 @@ import HorseCard from '@/components/HorseCard';
 import Link from 'next/link';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function ForSalePage({ searchParams }) {
   const gender = searchParams?.gender;
